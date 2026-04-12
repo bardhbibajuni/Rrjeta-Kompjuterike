1247b17d400b73363f043bb1b4879c0f1979f656
AndiLila1 <andi.lila@student.uni-pr.edu>
2026-04-12 21:29:28 +0200

    shtimi i logging dhe permiresime ne kod

--- Server.js
+++ Server.js
@@ -8,11 +8,17 @@ const server = dgram.createSocket("udp4");
 
 server.on("message", (msg, rinfo) => {
     const text = msg.toString();
+    
+    console.log("\n==============================");
+    console.log(`IP: ${rinfo.address}`);
+    console.log(`PORT: ${rinfo.port}`);
+    console.log("==============================");
 
-    console.log(`Client connected: ${rinfo.address}:${rinfo.port}`);
-    console.log("Message:", text);
 
-    const response = text.toUpperCase();
+    console.log(`Client i lidhur: ${rinfo.address}:${rinfo.port}`);
+    console.log("Mesazhi:", text);
+
+    const response = `Serveri ka pranuar: ${text}`;
     server.send(response, rinfo.port, rinfo.address);
 });
 
