32593a5a038eec42388c4d73755c6d346e4162d3
bardhbibajuni <bardh.bibaj@student.uni-pr.edu>
2026-04-16 17:13:06 +0200

    Update Client.js

---
+++ Client.js
@@ -0,0 +1,13 @@
+
+//  UDP CLIENT - Group 7 | JavaScript
+//  Rrjeta Kompjuterike 2025/26
+
+
+const dgram    = require("dgram");
+const readline = require("readline");
+const os       = require("os");
+
+
+const SERVER_PORT = 4444;
+const SERVER_HOST = "127.0.0.1";  // me ndrru ip me e shkru te fk
+const CLIENT_PORT = 5000;         
